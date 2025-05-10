3f89ecdc53c4fd5d8555297fdf538b0c00ca5fae
Sandi Sharoi <sandisharoi18@gmail.com>
2025-05-10 18:54:07 +0630

    Extract Jobs from Foundit(SG) and update main.py

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from jobnetmm import JobNetScraper
 from jobdbsg import JobsDBScraper
+from foundit import FounditScraper
 import os
 
 def main():
     ## For jobnetmm
-    email = os.getenv("JOBNET_EMAIL")
-    password = os.getenv("JOBNET_PASSWORD")
-    job_function = 17 # 17 for IT jobs
-    extract = JobNetScraper(email, password)
-    jobnet_df = extract.get_jobs(job_function=job_function)
-    print(jobnet_df.head())
+    # email = os.getenv("JOBNET_EMAIL")
+    # password = os.getenv("JOBNET_PASSWORD")
+    # job_function = 17  # 17 for IT jobs
+    # extract = JobNetScraper(email, password)
+    # jobnet_df = extract.get_jobs(job_function=job_function)
+    # print(jobnet_df.head())
 
-    ## For jobsdb_sg
-    print("🔍 Scraping Software Developer job from JobsDB Singapore...")
-    jobsdb_scraper = JobsDBScraper(max_pages=10, headless=True)
-    jobdbsg_df = jobsdb_scraper.run()
-    print(jobdbsg_df.head())
-    print("✅ JobsDB scraping complete.")
+    # ## For jobsdb_sg
+    # print("🔍 Scraping Software Developer job from JobsDB Singapore...")
+    # jobsdb_scraper = JobsDBScraper(max_pages=10, headless=True)
+    # jobdbsg_df = jobsdb_scraper.run()
+    # print(jobdbsg_df.head())
+    # print("✅ JobsDB scraping complete.")
 
-    ## For foundit
+
+    # # For foundit
+    print("🔍 Scraping IT jobs from Foundit Singapore...")
+    foundit_scraper = FounditScraper(headless=True)
+    foundit_df = foundit_scraper.run()
+    print(foundit_df.head())
+    print("✅ Foundit scraping complete.")
 
 if __name__ == "__main__":
     main()
